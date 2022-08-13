3006c4e09cbb543966eeab1af55453c6fab31562
Maobal <65205689+Maobal@users.noreply.github.com>
2022-08-13 15:02:03 -0500

    SOLUCION RETO 5 DEL GRUPO 55

---
+++ reto_5_G55/src/main/java/com/mycompany/reto_5_g66/controlador/ControladorMain.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.reto_5_g66.controlador;
+
+import com.mycompany.reto_5_g66.vista.frmPrincipal;
+
+
+
+
+public class ControladorMain {
+    public static void main( String[] args ){
+     new frmPrincipal().setVisible(true);
+        
+        
+    }
+    
+}
